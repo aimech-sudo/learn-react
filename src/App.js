6f5ed340ef4d2d115e04c7292638709fb9d8ea79
aimech-sudo <dkskzmfhshtm@gmail.com>
2026-09-16 13:21:50 +0900

    리액트 콤포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import Greeting from "./Greeting";
 
 function App() {
+  // logic
+  // 구조분해할당 - object destructuring
+  const animals = ["dog", "cat", "pig"];
+  // const first = animals[0]
+  // console.log("🚀 ~ App ~ first:", first)
+  // const second = animals[1]
+  // console.log("🚀 ~ App ~ second:", second)
+  
+  const [ second, first ] = animals
+  console.log("🚀 ~ App ~ first:", first)
+  console.log("🚀 ~ App ~ second:", second)
+
+  const todoItem = {
+    todo: "React 학습하기",
+    isComplete: false
+  }
+
+  const { todo, isComplete}  = todoItem
+  console.log("🚀 ~ App ~ isComplete:", isComplete)
+  console.log("🚀 ~ App ~ todo:", todo)
+
+  // view
   return (
     <div className="App">
       필수 익스텐션
- * 1. react snippet
- * 2. auto import
- * 3. tailwind css intellicense
+      <Greeting />
+      <Greeting />
+      <Greeting />
+      <Greeting />
     </div>
   );
 }
---
+++ src/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Greeting = () => {
+  return (
+    <div style={{"backgroundColor": "pink", "padding": "10px"}}>
+      Greeting
+      
+    </div>
+  )
+}
+
+export default Greeting
