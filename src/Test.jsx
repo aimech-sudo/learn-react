821dc84c0d41e87a14a75faacb540a44d04f3ffd
aimech-sudo <dkskzmfhshtm@gmail.com>
2026-09-16 11:01:29 +0900

    first commit

---
+++ src/Test.jsx
@@ -0,0 +1,15 @@
+/**
+ * 필수 익스텐션
+ * 1. react snippet
+ * 2. auto import
+ * 3. tailwind css intellicense
+ */
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>Test</div>
+  )
+}
+
+export default Test
